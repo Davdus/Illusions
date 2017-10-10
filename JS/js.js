d6402873a1d7aa86f4cd1777ec9785b1e56061f2
Diego <diego.schleis29@gmail.com>
2017-10-10 10:45:04 +0200

    JQuery import and slide effect, beginning of menu bar

--- JS/js.js
+++ JS/js.js
@@ -1,8 +1,18 @@
-$('<img/>').hide().attr('src', '../IMG/startimeffects.png').load(function(){
-    $('startimg').append($(this));
-});
-$('#fb').hover(function() {
-    $('input[type=button]').css('background-image', 'url("images/background.jpg")');
-}, function() {
-    $('startimg').css('background', '');
-});
+function scrollToAnchor(aid, aidOffset){
+    var aTag = $("div[id='"+ aid +"']");
+    $('html,body').animate(
+        {
+            scrollTop: aTag.offset().top+aidOffset+1
+        },'slow');
+}
+(function($) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 200) {
+                $('#menu').fadeIn(500);
+            } else {
+                $('#menu').fadeOut(500)
+            }
+        });
+    });
+})(jQuery);
