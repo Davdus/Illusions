4ce150aab36d48057e950ccd1fcd34755dcc70b1
Diego <diego.schleis29@gmail.com>
2018-01-10 10:15:49 +0100

    For David

--- JS/js.js
+++ JS/js.js
@@ -1,18 +1,59 @@
-function scrollToAnchor(aid, aidOffset){
-    var aTag = $("div[id='"+ aid +"']");
+var menuhidden = false;
+
+function scrollToAnchor(aid, aidOffset) {
+    var aTag = $("*[id='" + aid + "']");
     $('html,body').animate(
         {
-            scrollTop: aTag.offset().top+aidOffset+1
-        },'slow');
+            scrollTop: aTag.offset().top + aidOffset + 1
+        }, 'slow');
 }
-(function($) {
-    $(document).ready(function(){
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 200) {
-                $('#menu').fadeIn(500);
-            } else {
-                $('#menu').fadeOut(500)
-            }
-        });
+
+$(document).ready(function () {
+    var menu = $('#menu');
+    var body = $('#body');
+
+    function scrollTopHigh(){
+        console.log(menuhidden);
+        if(menuhidden === false) {
+            body.css({"overflow": "hidden"});
+            menu.css({"position": "absolute", "top": $(this).scrollTop()});
+            menu.fadeIn(500);
+        }
+    }
+
+    function scrollTopHigherThanBody(){
+        menu.css({"position": "relative", "backgroundColor": "yellow"});
+    }
+
+    function scrollTopLow(){
+        body.css({"overflow": "hidden"});
+        menu.fadeOut(500);
+    }
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            scrollTopHigh();
+        } else if ($(this).scrollTop() > $('#body').height) {
+            scrollTopHigherThanBody();
+        } else {
+            scrollTopLow();
+        }
+
+        if ($(this).scrollTop() > $('#slideto').scrollTop()){
+            body.css({"overflow": "auto"});
+        } else {
+            body.css({"overflow": "hidden"});
+        }
     });
-})(jQuery);
+
+    $('#backButton').on('click', function () {
+        scrollTopLow();
+        menuhidden = true;
+        setTimeout(function() {
+            scrollToAnchor('body', 0);
+        }, 500);
+        setTimeout(function() {
+            menuhidden = false;
+        }, 2000);
+    });
+});
